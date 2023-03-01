5381fbd6ceef85ed7551fd5760955bcd3abd83d8
jamseyboy <buongahrangchal@gmail.com>
2023-03-01 23:17:38 +0530

    task3 first commit

--- James_CourseAssignment.sh
+++ James_CourseAssignment.sh
@@ -21,5 +21,7 @@ tar cvzf ${myname}-httpd-logs-${date_time}.tar
 aws s3  cp  ${myname}-httpd-logs-${date_time}.tar s3://4{s3_bucket}/${myname}-httpd-logs-${date_time}.tar
 echo " Tar uploaded to s3 bucket" 
 
-cat  inventory.html
 echo "httpd-logs ${date} tar ${size}>>index.html"
+
+crontab -e 
+00 10 * * * https://github.com/jamseyboy/james_course_assignment/blob/c330cf3cfaf7a399bcfaf2893c6f7f682bdc523c/James_CourseAssignment.sh
